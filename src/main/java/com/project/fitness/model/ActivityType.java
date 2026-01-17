84cfb0ae5c0aa87c80df0ff2259e782ebabe3c68
prajjawal12 <prajjawaldy.zhcet@gmail.com>
2026-01-17 13:04:15 +0530

    first commit

---
+++ src/main/java/com/project/fitness/model/ActivityType.java
@@ -0,0 +1,42 @@
+package com.project.fitness.model;
+
+public enum ActivityType {
+    // Cardio
+    RUNNING,
+    WALKING,
+    CYCLING,
+    TREADMILL,
+    ELLIPTICAL,
+    STAIR_CLIMBING,
+    ROWING,
+    JUMP_ROPE,
+
+    // Strength & Training
+    WEIGHT_TRAINING,
+    BODYWEIGHT_TRAINING,
+    CROSSFIT,
+    FUNCTIONAL_TRAINING,
+    POWERLIFTING,
+
+    // Sports
+    FOOTBALL,
+    BASKETBALL,
+    CRICKET,
+    TENNIS,
+    BADMINTON,
+    SWIMMING,
+    BOXING,
+    MARTIAL_ARTS,
+
+    // Mind & Flexibility
+    YOGA,
+    PILATES,
+    STRETCHING,
+    MEDITATION,
+
+    // Lifestyle / Other
+    HIKING,
+    DANCING,
+    AEROBICS,
+    GENERAL_WORKOUT
+}
